72c208478402cef64a2d83d1cec16f25f854fd5b
Dany Derigon <dany.derigon@gmail.com>
2023-12-16 10:44:21 +0400

    fixes

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,7 +1,6 @@
-<div id="livewire-bootstrap-modal" class="modal"  tabindex="-1" role="dialog" aria-hidden="true"
-     wire:ignore.self
-    style="z-index: 9999;">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
+<div id="livewire-bootstrap-modal" class="modal"  tabindex="-1" role="dialog" aria-hidden="true" wire:ignore.self
+     style="z-index: 9999;">
+    <div class="modal-dialog {{$class}}">
         <div class="modal-content">
             @if ($alias)
                 @livewire($alias, $params, key($activemodal))
--- src/Components/Modals.php
+++ src/Components/Modals.php
@@ -11,15 +11,19 @@ class Modals extends Component
     public $alias;
     public $params = [];
     public $activemodal;
+    public $class;
+
 
     #[On('showModal')]
-    public function showModal(string $alias, array $params = [])
+    public function showModal(string $alias, array $params = [], string $class = 'modal-dialog-centered modal-lg')
     {
         $this->alias = $alias;
         $this->params = $params;
+        $this->class = $class;
         $this->activemodal = rand();
         $this->dispatch('showBootstrapModal');
     }
+
 
     #[On('resetModal')]
     public function resetModal()

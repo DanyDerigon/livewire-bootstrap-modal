7e3331d481f7e0348f49b4eda4f483e299ac028a
Dany Derigon <dany.derigon@gmail.com>
2023-12-16 05:37:20 +0400

    fixes

--- resources/views/modals.blade.php
+++ resources/views/modals.blade.php
@@ -1,4 +1,6 @@
-<div id="livewire-bootstrap-modal" class="modal"  tabindex="-1" role="dialog" aria-hidden="true" wire:ignore.self
+<div id="livewire-bootstrap-modal" class="modal"  tabindex="-1" role="dialog" aria-hidden="true"
+     data-bs-backdrop="static" data-bs-keyboard="false"
+     wire:ignore.self
     style="z-index: 9999;">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
